1c0e03dc9c175346be15e3657dbbdff799b829d0
stefanpython <99439572+stefanpython@users.noreply.github.com>
2024-11-21 11:55:44 +0200

    Add links to signup and login buttons (#44)

--- frontend/app/components/layout/navbar.tsx
+++ frontend/app/components/layout/navbar.tsx
@@ -9,21 +9,24 @@ import {
 } from "@/components/ui/navigation-menu";
 import logo from "../../public/text_logo.png";
 import { Share2 } from "lucide-react";
-import user  from "../../public/user.png"
-import { cn } from "@/lib/utils"
+import user from "../../public/user.png";
+import { cn } from "@/lib/utils";
+import Link from "next/link";
 
 interface NavbarProps extends React.HTMLAttributes<HTMLDivElement> {}
-const Navbar : React.FC<NavbarProps> = ({ className, ...props }) => {
+const Navbar: React.FC<NavbarProps> = ({ className, ...props }) => {
   return (
-    <div className={cn('w-full bg-navbardark flex items-center justify-between px-6 h-16',className)} {...props}>
+    <div
+      className={cn(
+        "w-full bg-navbardark flex items-center justify-between px-6 h-16",
+        className
+      )}
+      {...props}
+    >
       {/* Logo and Text */}
       <div className="flex items-center space-x-2">
         <span className="text-white text-lg font-semibold">
-          <Image
-            src={logo}
-            alt="logo"
-            className="inline-block"
-          />
+          <Image src={logo} alt="logo" className="inline-block" />
         </span>
       </div>
 
@@ -39,25 +42,26 @@ const Navbar : React.FC<NavbarProps> = ({ className, ...props }) => {
             </NavigationMenuLink>
           </NavigationMenuItem>
 
-          {/* Log in Button with User Icon */}
+          {/* Signp Up Button */}
           <NavigationMenuItem>
             <NavigationMenuLink asChild>
-              <a href="/signup" className="flex items-center gap-2 text-white">
+              <Link
+                href="/sign-up"
+                className="flex items-center gap-2 text-white"
+              >
                 Sign up
-              </a>
+              </Link>
             </NavigationMenuLink>
           </NavigationMenuItem>
 
-          {/* Sign up Button */}
+          {/* Login Button */}
           <NavigationMenuItem>
             <NavigationMenuLink asChild>
-              <Button className="bg-purplev1 text-black px-4 py-2 rounded-lg">
-                <Image
-                 src={user}
-                 alt="usericon"
-                 className="h-5 w-5" 
-                 />
-              </Button>
+              <Link href="login">
+                <Button className="bg-purplev1 text-black px-4 py-2 rounded-lg">
+                  <Image src={user} alt="usericon" className="h-5 w-5" />
+                </Button>
+              </Link>
             </NavigationMenuLink>
           </NavigationMenuItem>
         </NavigationMenuList>
