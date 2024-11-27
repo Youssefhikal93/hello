7b24d08066b2911f8ea38c37e251f3f0c577dfe2
stefanpython <99439572+stefanpython@users.noreply.github.com>
2024-11-27 16:08:24 +0200

    Integration of forget/reset password pages with backend api endpoints (#52)
    Added backend API calls for forget/reset password pages

--- frontend/app/components/popup/LoginForm.tsx
+++ frontend/app/components/popup/LoginForm.tsx
@@ -94,7 +94,25 @@ export default function LoginForm() {
     } catch (error) {
       // Handle any errors that occur during the API call
       console.error("Login error:", error);
-      setError("An error occurred while logging in. Please try again later.");
+
+      // Extract the error message
+      let errorMessage =
+        "An error occurred while logging in. Please try again.";
+
+      if (error instanceof Error) {
+        if (
+          "response" in error &&
+          error.response &&
+          typeof error.response === "object"
+        ) {
+          errorMessage =
+            (error.response as any)?.data?.message || error.message;
+        } else {
+          errorMessage = error.message; // Fallback to the generic error message
+        }
+      }
+
+      setError(errorMessage);
     }
   };
 
@@ -111,12 +129,14 @@ export default function LoginForm() {
     <div className="min-h-screen flex flex-col ">
       {/* Navbar */}
       <div className="flex justify-center items-center h-[55px] bg-white">
-        <Image
-          src={navLogo}
-          alt="Logo linking to the homepage"
-          className="w-[40px] h-[40px]"
-          priority
-        />
+        <Link href="/">
+          <Image
+            src={navLogo}
+            alt="Logo linking to the homepage"
+            className="w-[40px] h-[40px]"
+            priority
+          />
+        </Link>
       </div>
 
       <hr className="border-gray-300" />
